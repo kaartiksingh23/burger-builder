ea231338986f34c9a493977c8e7cb27b269af981
kaartiksingh23 <kaartiksingh23@gmail.com>
2020-04-07 00:01:55 +0530

    removed unnecessary comments

--- src/Components/Cockpit/Cockpit.js
+++ src/Components/Cockpit/Cockpit.js
@@ -1,32 +1,31 @@
-import React from 'react';
-import classes from '../Cockpit/Cockpit.css';
+import React from "react";
+import classes from "../Cockpit/Cockpit.css";
 
 const Cockpit = (props) => {
+  let btnClass = [classes.Button];
 
-    let btnClass = [classes.Button]
+  if (props.showPersons) {
+    btnClass.push(classes.Red);
+  }
 
-    if (props.showPersons) {
-        btnClass.push(classes.Red);
-    }
+  //let classes = ['red','bold'].join(' '); //converts array of string to "red bold"
+  const assignedClasses = [];
+  if (props.persons.length <= 2) {
+    assignedClasses.push(classes.red);
+  }
+  if (props.persons.length <= 1) {
+    assignedClasses.push(classes.bold);
+  }
 
-    //let classes = ['red','bold'].join(' '); //converts array of string to "red bold"
-    const assignedClasses = [];
-    if(props.persons.length<=2){
-      assignedClasses.push(classes.red);
-    }
-    if(props.persons.length<=1){
-      assignedClasses.push(classes.bold);
-    }
-
-    return (
+  return (
     <div>
-        <h1>Hi, Im a React App</h1>
-        <p className={assignedClasses.join(" ")}>This is really working</p>
-        <button className= {btnClass.join(" ")} onClick={props.click}>
+      <h1>Hi, Im a React App</h1>
+      <p className={assignedClasses.join(" ")}>This is really working</p>
+      <button className={btnClass.join(" ")} onClick={props.click}>
         Toggle Person
-        </button>
+      </button>
     </div>
-    )
-}
+  );
+};
 
-export default Cockpit
+export default Cockpit;
--- src/Containers/App.js
+++ src/Containers/App.js
@@ -1,45 +1,38 @@
 import React, { Component } from "react";
-import classes from './App.css';
-// import Person from "../Components/Persons/Person/Person";
-// import "./App.css";
-
-// import Radium, { StyleRoot} from 'radium';
-// import ErrorBoundary from '../ErrorBoundary/ErrorBoundary'
-
-import Persons from '../Components/Persons/Persons';
-import Cockpit from '../Components/Cockpit/Cockpit';
+import classes from "./App.css";
+import Persons from "../Components/Persons/Persons";
+import Cockpit from "../Components/Cockpit/Cockpit";
 
 // Class Component
 class App extends Component {
   state = {
     persons: [
-      { id:'asfa1', name: "Max", age: 28 },
-      { id:'asfa2', name: "Manu", age: 30 },
-      { id:'asfa3', name: "Stephanie", age: 26 }
+      { id: "asfa1", name: "Max", age: 28 },
+      { id: "asfa2", name: "Manu", age: 30 },
+      { id: "asfa3", name: "Stephanie", age: 26 },
     ],
-    showPersons: false
+    showPersons: false,
   };
 
-  deletePersonHandler = personIndex => {
+  deletePersonHandler = (personIndex) => {
     // const persons = this.state.persons.slice(); //creates the copy , Remember arrays and objects are reference types
     const persons = [...this.state.persons]; // ES6 way for making the copy
     persons.splice(personIndex, 1);
     this.setState({ persons: persons });
   };
 
-  nameChangedHandler = ( event, id ) => {
-    
-    const personIndex = this.state.persons.findIndex(p => {
-      return p.id === id; 
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex((p) => {
+      return p.id === id;
     });
 
-    const person = {...this.state.persons[personIndex]}; //creates a copy
-    
+    const person = { ...this.state.persons[personIndex] }; //creates a copy
+
     person.name = event.target.value;
     const persons = [...this.state.persons];
-    persons[personIndex] = person ;
+    persons[personIndex] = person;
 
-    this.setState({persons: persons})
+    this.setState({ persons: persons });
   };
 
   togglePersonsHandler = () => {
@@ -49,88 +42,33 @@ class App extends Component {
 
   render() {
     //this way styles are scoped to component , this is called inline styles, but this way you will not able to leverage the full power of css
-   
 
     let persons = null;
-   
 
     if (this.state.showPersons) {
-
       persons = (
         <div>
-          <Persons clicked={this.deletePersonHandler} persons={this.state.persons} changed={this.nameChangedHandler}></Persons>
+          <Persons
+            clicked={this.deletePersonHandler}
+            persons={this.state.persons}
+            changed={this.nameChangedHandler}
+          ></Persons>
         </div>
       );
     }
 
     return (
-      
-
-        <div className={classes.App}>
-          <Cockpit showPersons={this.state.showPersons} persons={this.state.persons} click={this.togglePersonsHandler}></Cockpit>
-          {persons}
-        </div>
-
-        
-      
+      <div className={classes.App}>
+        <Cockpit
+          showPersons={this.state.showPersons}
+          persons={this.state.persons}
+          click={this.togglePersonsHandler}
+        ></Cockpit>
+        {persons}
+      </div>
     );
-
-    // The above code is compiled to below code by many workflow build tools we get out of this project
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1',null,'Hi, I\'m a React APP'))
-
-    /*
-      Restrictions in jsx =>
-        class => ClassName
-        JSX elements should have a root element .... But adjacent elements can be achieved ...Remember!
-    */
   }
 }
 
 // export default Radium(App); //higher order component
 export default App;
-// import React, { useState } from "react";  //UseState is a React Hook. Hooks add functionality to functional component.
-// //UseState allows to add state management in functional components
-// import Person from "./Person/Person";
-// import './App.css';
-
-// const App = () => {
-//   const [personsState, setPersonsState]  = useState({  //ARRAY DESTRUCTURING
-//     persons: [
-//       { name: "Max", age: 28 },
-//       { name: "Manu", age: 30 }
-//     ],
-//     otherState: "Some Value"
-//   });
-
-//   const switchNameHandler = () => {
-//     setPersonsState({ //Remember : it does not update state it replaces it
-//       persons: [
-//         { name: "Kaartik", age: 27 },
-//         { name: "Manu", age: 31 }
-//       ],
-//       otherState: personsState.otherState
-//     });
-
-//     console.log(this.st`ate);
-//   };
-
-//   return (
-//     <div className="App">
-//       <h1>Hi, Im a React App</h1>
-//       <button onClick={switchNameHandler}>Switch Name</button>
-//       <Person
-//         name={personsState.persons[0].name}
-//         age={personsState.persons[0].age}
-//       />
-//       <Person
-//         name={personsState.persons[1].name}
-//         age={personsState.persons[1].age}
-//       >
-//         My Hobbies: Racing
-//       </Person>
-//     </div>
-//   );
-
-// };
-
-// export default App;
